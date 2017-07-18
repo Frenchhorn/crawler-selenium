5d276a8e9573616cb500f98f0e7f432140ffcced
Frenchhorn <nyanpasspass@outlook.com>
2017-07-18 18:40:43 +0800

    Edit download function

--- download.py
+++ download.py
@@ -9,10 +9,7 @@ def download(url):
     assert website_object, "Don't match"
     browser = utils.get_browser()
     website_object.set_browser(browser)
-    if website_object.menu_url:
-        pass
-    elif website_object.page_url:
-        pass
+    website_object.download()
 
 
 if __name__ == '__main__':
--- websites/base.py
+++ websites/base.py
@@ -31,8 +31,8 @@ class Base:
 
     # Below attribute should be override
     # Match
-    menu = None   # URL pattern
-    page = None   # URL pattern
+    menu = None         # URL pattern
+    page = None         # URL pattern
 
     # Menu page
     episode_name = ''   # locator
@@ -41,7 +41,6 @@ class Base:
     # View page
     image = ''          # locator
     next_page = ''      # locator
-    next_episode = ''   # locator
 
 
     def __init__(self, menu_url='', page_url='', browser=None):
@@ -49,28 +48,70 @@ class Base:
         self.page_url = page_url
         self.browser = browser
 
+
+    #######################
+    #       Setting       #
+    #######################
     def set_browser(self, browser):
         self.browser = browser
 
-    def get_episodes(self):
+    def _set_episodes(self):
+        elements = self.browser.find_elements_by_css_selector(self.episode)
+        for i in elements:
+            self.episodes.append(i.get_attribute('href'))
+
+
+    #######################
+    #      Download       #
+    #######################
+    def download(self):
         pass
 
-    def download(self, page_url):
-        pass
+    def _download_episodes(self):
+        for episode in self.episodes:
+            self.browser.get(episode)
+            self._download_episode()
 
+    def _download_episode(self):
+        self.download_page()    # first page
+        while self._next_page():
+            self._download_page()
+
+    def _download_page(self):
+        data = self._get_data_url()
+        self._save_as_file(data, 'test.png')
+
+    # for test
     def download_page(self, page_url):
         self.browser.get(page_url)
-        data = self.get_data_url()
-        self.save_as_file(data, 'test.png')
+        data = self._get_data_url()
+        self._save_as_file(data, 'test.png')
 
-    def get_data_url(self):
+
+    #######################
+    #       Action        #
+    #######################
+    def _next_page(self):
+        next_page_button = self.find_elements_by_css_selector(self.next_page)
+        if len(next_page_button) == 0:
+            return False
+        next_page_button = next_page_button[0]
+        if next_page_button.is_displayed():
+            next_page_button.click()
+            return True
+
+
+    #######################
+    #     Save Image      #
+    #######################
+    def _get_data_url(self):
         data_base64 = self.browser.execute_script('return (%s)(arguments[0])' % self.getDataURL_js, self.image)
         if not data_base64:
             self.LOGGER.error("Don't get data url")
         self.LOGGER.debug('Get data URL %s ...' % data_base64[:50])
         return data_base64[22:]
 
-    def save_as_file(self, data_base64, path):
+    def _save_as_file(self, data_base64, path):
         data = base64.b64decode(data_base64.encode('ascii'))
         with open(path, 'wb') as f:
             f.write(data)
